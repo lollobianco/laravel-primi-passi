0319d5a58a5a3227a58a1d4c744f8f1659ce73b8
lollobianco <loreb2002@gmail.com>
2023-01-11 16:20:07 +0100

    Creato passaggio tra le pagine

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -4,11 +4,11 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Home</title>
+   <title>Faq</title>
 </head>
 <body>
 
-   <h1 class="text-center">HOME</h1>
+   <h1 class="text-center">FAQ</h1>
 
    <div class="container d-flex">
 
